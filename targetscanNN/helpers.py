de7c4d451ed5dee0824a49d82f5b049d1060ce57
bluebandedgobi <bluebandedgobi9353@gmail.com>
2017-01-13 17:43:22 -0500

    adds ability to use extra features

--- targetscanNN/helpers.py
+++ targetscanNN/helpers.py
@@ -2,68 +2,38 @@ import numpy as np
 import tensorflow as tf
 
 
-def generate_random_sequence(length):
-    """Generate a random RNA sequence of a given length"""
+## CLASSES ##
+
+class Dataset(object):
+    """create dataset object that holds features and labels and cycles through the data in batches"""
+
+    def __init__(self, features, extra_features, labels):
+        assert (len(features) == len(labels))
+        self.features = np.array(features)
+        self.extra_features = np.array(extra_features)
+        self.labels = np.array(labels)
+        self.index = 0
+        self.size = len(labels)
     
-    nts = ['A','U','C','G']
-    sequence = np.random.choice(nts, size=length, replace=True)
-
-    return ''.join(sequence)
-
-
-def get_complementary(seq):
-    """Get the complementary sequence of a given RNA sequence"""
+    def next_batch(self, batch_size):
+        old_index = self.index
+        new_index = self.index + batch_size
+        self.index = new_index % self.size
+        if new_index <= self.size:
+            return (self.features[old_index: new_index],
+                    self.extra_features[old_index: new_index],
+                    self.labels[old_index: new_index])
+        else:
+            subfeatures = np.concatenate([self.features[old_index:], self.features[:self.index]])
+            subextra_features = np.concatenate([self.extra_features[old_index:], self.extra_features[:self.index]])
+            sublabels = np.concatenate([self.labels[old_index:], self.labels[:self.index]])
+            return (subfeatures, subextra_features, sublabels)
     
-    intab = "AUCG"
-    outtab = "UAGC"
-    trantab = str.maketrans(intab, outtab)
-
-    return seq.translate(trantab)
+    def reset_index(self):
+        self.index = 0
 
 
-def generate_match_pair(length, random_seed=None):
-    """Generate two sequences that are base-paired"""
-    
-    if random_seed is not None:
-        np.random.seed(random_seed)
-
-    seq1 = generate_random_sequence(length)
-    seq2 = get_complementary(seq1)
-    
-    return seq1, seq2
-
-
-def generate_seed_match_pair(length1, length2, random_seed=None):
-    """Generate two sequences that are base-paired at positions 1-7"""
-    
-    if random_seed is not None:
-        np.random.seed(random_seed)
-
-    seq1 = generate_random_sequence(length1)
-    up_fragment = generate_random_sequence(1)
-    down_fragment = generate_random_sequence(length2-7)
-    mid_fragment = get_complementary(seq1[1:7])
-    
-    seq2 = up_fragment + mid_fragment + down_fragment
-    
-    return seq1, seq2
-
-
-def generate_random_pair(length1, length2, random_seed=None):
-    """Generate two random sequences that are not perfectly complementary"""
-    
-    if random_seed is not None:
-        np.random.seed(random_seed)
-    
-    seq1 = generate_random_sequence(length1)
-    match_seq1 = get_complementary(seq1)
-
-    while True:
-        seq2 = generate_random_sequence(length2)
-
-        if match_seq1 != seq2:
-            return seq1, seq2
-
+## Data Functions ##
 
 def one_hot_encode(seq, nt_order):
     """Convert RNA sequence to one-hot encoding"""
@@ -80,4 +50,155 @@ def make_square(seq1, seq2):
     return np.outer(one_hot_encode(seq1, np.array(['A','U','C','G'])),
                     one_hot_encode(seq2, np.array(['U','A','G','C'])))
 
+
+def read_data(infile, len_data, len_features, extra=None, shuffle=True):
+    """Reads in sequences and creates dataset objects"""
+
+    # create arrays to hold data
+    features = np.zeros((len_data, len_features))
+    labels = np.zeros((len_data, 1))
+
+    if extra is not None:
+        extra_features = np.zeros((len_data, extra))
+
+    else:
+        extra_features = np.zeros((len_data, 1))
+        extra_features.fill(None)
+
+    # read in sequences and use 'make square' encoding
+    with open(infile, 'r') as f:
+        i = 0
+        for line in f:
+            line = line.split(',')
+            features[i, :] = make_square(line[0],line[1]).flatten()
+            labels[i,:] = [float(line[2])]
+            if extra:
+                extra_features[i, :] = [float(x) for x in line[3:]]
+            i += 1
+
+        assert (i == len_data)
+
+    # shuffle data
+    if shuffle:
+        ix = np.arange(len_data)
+        np.random.shuffle(ix)
+
+        features = features[ix, :]
+        extra_features = extra_features[ix, :]
+        labels = labels[ix, :]
+
+    # create Dataset objects
+    test_size = int(len(features)/10)
+    train = Dataset(features[test_size:], extra_features[test_size:], labels[test_size:])
+    test = Dataset(features[:test_size], extra_features[:test_size], labels[:test_size])
+
+    return train, test
+
+
+## NN Functions ##
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def variable_summaries(var):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
+
+def make_convolution_layer(input_tensor, dim1, dim2, in_channels, out_channels, stride1, stride2,
+    layer_name, padding='SAME', act=tf.nn.relu):
+    """Create layer, given the input tensor, dimensions, and preactivation function
+    """
+    # add a name scope ensures logical grouping of the layers in the graph.
+    with tf.name_scope(layer_name):
+        # create variables for weights and biases
+        with tf.name_scope('weights'):
+            weights = weight_variable([dim1, dim2, in_channels, out_channels])
+            variable_summaries(weights)
+        with tf.name_scope('biases'):
+            biases = bias_variable([out_channels])
+            variable_summaries(biases)
+        with tf.name_scope('Wx_plus_b'):
+            preactivate = tf.nn.conv2d(input_tensor, weights, strides=[1, stride1, stride2, 1], padding=padding) + biases
+            tf.summary.histogram('pre_activations', preactivate)
+        
+        out_layer = act(preactivate, name='activation')
+        tf.summary.histogram('activations', out_layer)
+        
+        return out_layer 
+
+def make_fullyconnected_layer(input_tensor, in_channels, out_channels, layer_name, act=tf.nn.relu):
+    """Create layer, given the input tensor, dimensions, and preactivation function
+    """
+    # add a name scope ensures logical grouping of the layers in the graph.
+    with tf.name_scope(layer_name):
+        # create variables for weights and biases
+        with tf.name_scope('weights'):
+            weights = weight_variable([in_channels, out_channels])
+            variable_summaries(weights)
+        with tf.name_scope('biases'):
+            biases = bias_variable([out_channels])
+            variable_summaries(biases)
+        with tf.name_scope('Wx_plus_b'):
+            preactivate = tf.matmul(input_tensor, weights) + biases
+            tf.summary.histogram('pre_activations', preactivate)
+        
+        out_layer = act(preactivate, name='activation')
+        tf.summary.histogram('activations', out_layer)
+        
+        return out_layer 
+
+
+def make_train_step(problem_type, tensor, y):
+    if problem_type == 'classification':
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(tensor, y))
+        with tf.name_scope('train'):
+            train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+        
+        correct_prediction = tf.equal(tf.argmax(tensor,1), tf.argmax(y,1))
+        
+        with tf.name_scope('accuracy'):
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        
+    elif problem_type == 'regression':
+        SS_err = tf.reduce_sum(tf.square(tf.sub(tensor, y)))
+        SS_tot = tf.reduce_sum(tf.square(tf.sub(y, tf.reduce_mean(y))))
+        R_2 = tf.sub(tf.cast(1.0, tf.float32), tf.div(SS_err, SS_tot))
+
+        with tf.name_scope('train'):
+            train_step = tf.train.AdamOptimizer(1e-4).minimize(SS_err)
+        with tf.name_scope('accuracy'):
+            accuracy = R_2
     
+    else:
+        print('problem_type must be \'classification\' or \'regression\'')
+        
+    tf.summary.scalar('accuracy', accuracy)
+
+    return train_step, accuracy
+
+# def fully_connected(tensor, weights, biases):
+#     """Preactivation function for a fully connected layer"""
+
+#     return tf.matmul(tensor, weights) + biases
+
+
+# def convolution(tensor, weights, biases, stride1, stride2, padding='SAME'):
+#     """Preactivation function for a fully connected layer"""
+
+#     return tf.nn.conv2d(tensor, weights, strides=[1, stride1, stride2, 1], padding=padding) + biases
--- targetscanNN/targetscanNN.py
+++ targetscanNN/targetscanNN.py
@@ -4,64 +4,130 @@ import numpy as np
 import tensorflow as tf
 
 import helpers
-import classes
+# import classes
 
 if __name__ == '__main__':
 
     INFILE, LEN_DATA, LEN_FEATURES, LOGDIR = sys.argv[1:]
     LEN_DATA, LEN_FEATURES = int(LEN_DATA), int(LEN_FEATURES)
+    WRITE_SUMMARY = False
 
-    print('Reading in data')
-
-    features = np.zeros((LEN_DATA, LEN_FEATURES))
-    labels = np.zeros((LEN_DATA, 1))
-
-    with open(INFILE, 'r') as f:
-        for i,line in enumerate(f):
-            line = line.split(',')
-            features[i, :] = helpers.make_square(line[0],line[1]).flatten()
-            labels[i,:] = [float(line[2])]
-
-    print('Shuffling data')
-
-    ix = np.arange(LEN_DATA)
-    np.random.shuffle(ix)
-
-    features = features[ix, :]
-    labels = labels[ix, :]
-
-    test_size = int(len(features)/10)
-    constructed_logfc_train = classes.Dataset(features[test_size:], labels[test_size:])
-    constructed_logfc_test = classes.Dataset(features[:test_size], labels[:test_size])
-
-    print('Training neural network')
+    print('Building neural network')
 
     with tf.Session() as sess:
 
         dim1, dim2 = 80, 96
-        num_output1 = 16
-        num_output2 = 16
-        fully_connected_nodes = 32
+        num_extra = 1
         out_nodes = 1
 
-        NN = classes.NeuralNet2D(sess, dim1, dim2, out_nodes)
-        NN.add_convolution('convolution1', 4, 4, 4, 4, num_output1)
-        NN.add_convolution('convolution2', 2, 2, 1, 1, num_output2)
-        NN.add_fully_connected('fullyconnected', fully_connected_nodes)
-        NN.add_dropout('dropout', out_nodes)
-        NN.make_train_step('regression', LOGDIR)
-        NN.train_model(constructed_logfc_train, constructed_logfc_test, num_epoch=10000,
-                       batch_size=100, report_int=1000, keep_prob_train=0.5)
+        # create placeholders for data
+        x = tf.placeholder(tf.float32, shape=[None, dim1*dim2])
+        extra = tf.placeholder(tf.float32, shape=[None, num_extra])
+        y = tf.placeholder(tf.float32, shape=[None, out_nodes])
+        x_image = tf.reshape(x, [-1, dim1, dim2, 1])
+        
+        # create summaries of features for visualizing with tensorboard
+        x_summary = tf.summary.image('images', x_image)
 
-        # dim1, dim2 = 16, 16
-        # num_output1 = 4
-        # fully_connected_nodes = 1024
-        # out_nodes = 2
+        ## LAYER 1 ##
 
-        # NN = classes.NeuralNet2D(sess, dim1, dim2, out_nodes)
-        # NN.add_convolution('convolution1', 4, 4, 4, 4, num_output1)
-        # NN.add_fully_connected('fullyconnected1', fully_connected_nodes)
-        # NN.add_dropout('dropout', out_nodes)
-        # NN.make_train_step('classification', '../../logdirs/match')
-        # NN.train_model(match_train, match_test, num_epoch=1000,
-        #                batch_size=50, report_int=10, keep_prob_train=0.5)
+        # convolution layer for each 4x4 box representing two paired nucleotides
+        in_channels = 1
+        out_channels = 16
+        layer1 = helpers.make_convolution_layer(x_image, 4, 4, in_channels, out_channels,
+                                                4, 4, 'layer1', padding='SAME', act=tf.nn.relu)
+
+        ## LAYER 2 ##
+
+        # convolution layer for each 2x2 box representing each dinucleotide
+        in_channels = out_channels
+        out_channels1 = 16
+        layer2 = helpers.make_convolution_layer(layer1, 2, 2, in_channels, out_channels1,
+                                                  1, 1, 'layer2_1', padding='SAME', act=tf.nn.relu)
+
+        # # make a parallel layer with for 6x6 box
+        # out_channels2 = 16
+        # layer2_2 = helpers.make_convolution_layer(layer1, 6, 6, in_channels, out_channels2,
+        #                                           1, 1, 'layer2_2', padding='SAME', act=tf.nn.relu)
+
+        # concatenate tensors:
+        # layer2 = tf.concat(3, [layer2_1, layer2_2])
+
+        # reshape to 1D tensor
+        layer2_flat_dim = layer2.get_shape().as_list()
+        layer2_flat_dim = layer2_flat_dim[1] * layer2_flat_dim[2] * layer2_flat_dim[3]
+        layer2_flat = tf.concat(1, [tf.reshape(layer2, [-1, layer2_flat_dim]), extra])
+
+        ## LAYER 3 ##
+
+        # add fully connected layer
+        in_channels = layer2_flat_dim + num_extra
+        out_channels = 32
+        layer3 = helpers.make_fullyconnected_layer(layer2_flat, in_channels, out_channels, 'fullyconnected', act=tf.nn.relu)
+
+        # add dropout
+        with tf.name_scope('dropout'):
+            keep_prob = tf.placeholder(tf.float32)
+            tf.summary.scalar('dropout_keep_probability', keep_prob)
+            dropout = tf.nn.dropout(layer3, keep_prob)
+
+        ## LAYER 4 ##
+
+        # add last layer with 1 output channel
+        in_channels = out_channels
+        out_channels = out_nodes
+        layer4 = helpers.make_fullyconnected_layer(dropout, in_channels, out_channels, 'fullyconnected', act=tf.identity)
+
+
+        # prepare training steps and log writers
+        train_step, accuracy = helpers.make_train_step('regression', layer4, y)
+
+        merged = tf.summary.merge_all()
+
+        if WRITE_SUMMARY:
+            train_writer = tf.summary.FileWriter(LOGDIR + '/train', sess.graph)
+            test_writer = tf.summary.FileWriter(LOGDIR + '/test')
+
+
+        print('Reading in data')
+
+        train, test = helpers.read_data(INFILE, LEN_DATA, LEN_FEATURES, extra=num_extra, shuffle=True)
+
+        print('Training model')
+
+        # TRAIN MODEL #
+        num_epoch = 10000
+        batch_size = 100
+        report_int = 100
+        keep_prob_train = 0.5
+
+        # initialize variables
+        sess.run(tf.global_variables_initializer())
+
+        # train epochs
+        for i in range(num_epoch):
+            batch = train.next_batch(batch_size)
+            if i%report_int == 0:
+                
+                acc, summary = sess.run([accuracy, merged], feed_dict={x: test.features,
+                                                                       extra: test.extra_features,
+                                                                       y: test.labels,
+                                                                       keep_prob: 1.0})
+                if WRITE_SUMMARY:
+                    test_writer.add_summary(summary, i)
+
+                print('Accuracy at step %s: %s' % (i, acc))
+            
+            else:
+                _, summary = sess.run([train_step, merged], feed_dict={x: batch[0],
+                                                                       extra: batch[1],
+                                                                       y: batch[2],
+                                                                       keep_prob: keep_prob_train})
+                if WRITE_SUMMARY:
+                    train_writer.add_summary(summary, i)
+
+        print("test accuracy %g"%accuracy.eval(feed_dict={x: test.features,
+                                                          extra: test.extra_features,
+                                                          y: test.labels,
+                                                          keep_prob: 1.0}))
+
